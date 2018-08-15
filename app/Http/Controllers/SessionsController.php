e34c7fef4bbce3fee639f0452600d800bdf087a8
Iwtrgw <Iwtrgw@Outlook.com>
2018-08-15 08:36:12 +0000

    增加用户的增删改查方法以及页面和样式

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,13 +7,24 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        //只让未登录用户访问登录页面
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
+    //登录页面
     public function create()
     {
         return view('sessions.create');
     }
 
+    //登录操作
     public function store(Request $request)
     {
+        //数据验证过滤
         $credentials = $this->validate($request,[
             'email' => 'required|email|max:255',
             'password' => 'required'
@@ -21,13 +32,14 @@ class SessionsController extends Controller
 
         if (Auth::attempt($credentials,$request->has('remember'))) {
             session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show',[Auth::user()]));
         } else {
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
     }
 
+    //退出
     public function destroy()
     {
         Auth::logout();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,21 +7,49 @@ use Illuminate\Http\Request;
 use Auth;
 
 class UsersController extends Controller {
+
+    public function __construct()
+    {
+        //未登录用户可访问
+        $this->middleware('auth',[
+            'except' => ['create','store','index']
+        ]);
+
+
+        //只让未登录用户访问注册页面
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
+    //注册页面
 	public function create() {
 		return view('users.create');
 	}
 
+    //用户个人中心
 	public function show(User $user) {
 		return view('users.show', compact('user'));
 	}
 
+
+    //注册动作
 	public function store(Request $request) {
+        //提交的数据验证
 		$this->validate($request, [
 			'name' => 'required|max:50',
 			'email' => 'required|email|unique:users|max:255',
 			'password' => 'required|confirmed|min:6',
 		]);
 
+        //创建用户
 		$user = User::create([
 			'name' => $request->name,
 			'email' => $request->email,
@@ -33,4 +61,47 @@ class UsersController extends Controller {
 
 		return redirect()->route('users.show', [$user]);
 	}
+
+    //用户编辑页面
+    public function edit(User $user)
+    {
+        //用户权限验证
+        $this->authorize('update',$user);
+
+        return view('users.edit',compact('user'));
+    }
+
+    //用户更新
+    public function update(User $user,Request $request)
+    {
+        //数据验证过滤
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        //权限验证
+        $this->authorize('update',$user);
+
+        //更新操作
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success','个人资料更新成功！');
+
+        return redirect()->route('users.show',$user->id);
+    }
+
+    //删除用户
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy',$user);
+        $user->delete();
+        session()->flash('success','成功删除用户！');
+        return back();
+    }
 }
